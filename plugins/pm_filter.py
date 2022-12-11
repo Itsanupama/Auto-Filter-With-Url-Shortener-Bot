671dd08b3f6c790136db638516bfd5b8a376af0a
botac135 <86068776+botac135@users.noreply.github.com>
2022-12-11 23:03:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -5,7 +5,7 @@ import ast
 import math
 from utils import get_shortlink
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-from Script import script
+from Script import script, RE_TEXT
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
