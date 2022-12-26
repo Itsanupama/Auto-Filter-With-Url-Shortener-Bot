a31f32cadb681b49ee109314e7ac8400e39ebc52
imtheprofessor <85482173+imtheprofessor@users.noreply.github.com>
2022-12-26 14:58:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/botac135/Movie-Bot-Url-shortener-bot /Eva
+  git clone https://github.com/imtheprofessor/Movie-Bot-Url-2 /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
